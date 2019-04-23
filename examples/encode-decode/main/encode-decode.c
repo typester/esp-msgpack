cb9d96c49e976e598b2c78ad387db6c5f106e5d3
Daisuke Murase <typester@cpan.org>
2019-04-24 05:21:23 +0900

    add Makefile.projbuild file

--- examples/encode-decode/main/encode-decode.c
+++ examples/encode-decode/main/encode-decode.c
@@ -1,26 +1,21 @@
-# esp-msgpack
-
-Message Pack components for ESP32 (esp-idf).
-
-## Usage
-
-1. clone or submodule this repo to `components/msgpack`.
-2. use!
-
-## Example app
-
-```c
 #include <stdio.h>
 #include <stdint.h>
+#include <assert.h>
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_log.h"
 
 #include "msgpack.h"
 
-void app_main() {
-    msgpack_sbuffer* buffer = msgpack_sbuffer_new();
-    msgpack_packer* pk = msgpack_packer_new(buffer, msgpack_sbuffer_write);
+static const char *TAG = "encode-decode";
+
+void app_main()
+{
+    ESP_LOGI(TAG, "app start");
+
+    msgpack_sbuffer *buffer = msgpack_sbuffer_new();
+    msgpack_packer *pk = msgpack_packer_new(buffer, msgpack_sbuffer_write);
 
     /* serializes ["Hello", "MessagePack", 12345]. */
     msgpack_pack_array(pk, 3);
@@ -34,19 +29,18 @@ void app_main() {
     msgpack_unpacked msg;
     msgpack_unpacked_init(&msg);
     msgpack_unpack_return ret = msgpack_unpack_next(&msg, buffer->data, buffer->size, NULL);
+    assert(ret == MSGPACK_UNPACK_SUCCESS);
 
     /* prints the deserialized object. */
     msgpack_object obj = msg.data;
-    msgpack_object_print(stdout, obj);  /*=> ["Hello", "MessagePack", 12345] */
+    msgpack_object_print(stdout, obj); /*=> ["Hello", "MessagePack", 12345] */
+    printf("\n");
 
     /* cleaning */
     msgpack_sbuffer_free(buffer);
     msgpack_packer_free(pk);
 
+    ESP_LOGI(TAG, "app end");
+
     vTaskDelete(NULL);
 }
-```
-
-## LICENSE
-
-MIT
